f6f9691213e07ed0258311698f1faea414e90813
Javiera Hidalgo <javiera_56@hotmail.com>
2022-08-22 15:50:20 -0400

    updating navbar info

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'about_me', to: 'home#about_me', as: 'about_me'
   get 'my_work', to: 'home#my_work', as: 'my_work'
+  get 'certificates', to: 'home#certificates', as: 'certificates'
+  get 'contact_me', to: 'home#contact_me', as: 'contact_me'
+
 end
